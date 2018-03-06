e2e006ae212a0ed902b5dab982644924de8db173
ezsilmar <zhirov@skbkontur.ru>
2018-03-06 13:23:52 +0500

    Simple client

--- Http2.WinHttpHandler/EntryPoint.cs
+++ Http2.WinHttpHandler/EntryPoint.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,17 +24,29 @@ namespace Http2.WinHttpHandler
 
             var mode = args[0];
             args = args.Skip(1).ToArray();
-            switch (mode)
+
+            try
             {
-                case "client":
-                    RunClient(args);
-                    break;
-                case "server":
-                    RunServer(args);
-                    break;
-                default:
-                    ShowHelp();
-                    break;
+                switch (mode)
+                {
+                    case "client":
+                        RunClient(args);
+                        break;
+                    case "server":
+                        RunServer(args);
+                        break;
+                    default:
+                        ShowHelp();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Out.WriteLine("Error occured: ");
+                Console.Out.WriteLine(ex);
+                Console.Out.WriteLine();
+                Console.Out.WriteLine();
+                ShowHelp();
             }
         }
 
@@ -54,7 +68,8 @@ namespace Http2.WinHttpHandler
         {
             using (var server = new HttpListener())
             {
-                server.Prefixes.Add($"http://*:54300/");
+                var listenPrefix = args[0];
+                server.Prefixes.Add(listenPrefix);
                 server.Start();
 
                 var ctxTask = server.GetContextAsync();
@@ -82,13 +97,48 @@ namespace Http2.WinHttpHandler
 
         private static void RunClient(string[] args)
         {
-            throw new NotImplementedException();
+            using (var httpClient = new HttpClient())
+            {
+                var url = args[0];
+                var parallelism = int.Parse(args[1]);
+                var tasks = new Task[parallelism];
+
+                for (var i = 0; i < parallelism; i++)
+                {
+                    tasks[i] = SendRequest(httpClient, url, i);
+                }
+
+                var allTasks = Task.WhenAll(tasks);
+                while (!cts.IsCancellationRequested)
+                {
+                    var delay = Task.Delay(50);
+                    var completed = Task.WhenAny(delay, allTasks).GetAwaiter().GetResult();
+                    if (completed == allTasks)
+                    {
+                        Console.WriteLine("All tasks finished!");
+                        break;
+                    }
+                }
+                
+                Console.WriteLine("Cancelling client...");
+            }
+        }
+
+        private static async Task SendRequest(HttpClient httpClient, string url, int idx)
+        {
+            var sw = Stopwatch.StartNew();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var result = await httpClient.SendAsync(request);
+            var elapsed = sw.Elapsed;
+            Console.WriteLine($"{idx}: code {(int) result.StatusCode:D3} in {elapsed.TotalSeconds:00}.{elapsed.Milliseconds:D3}");
         }
 
         private static void ShowHelp()
         {
             Console.Out.WriteLine(
-@"Usage: ");
+@"Usage: Http2.WinHttpHandler.exe <server|client> [args...]
+    server <listen prefix>
+    client <url> <parallelism>");
         }
 
         private static readonly CancellationTokenSource cts = new CancellationTokenSource();
